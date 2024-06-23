a0c335f7ce378d05bad6dfb9d24cdc82eab7f4f6
tbeleen <T.beleenmiranda@gmail.com>
2024-06-23 16:07:03 -0400

    inicial

--- mysite/settings.py
+++ mysite/settings.py
@@ -152,10 +152,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+STATICFILES_DIRS = [BASE_DIR / 'static']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # python manage.py collectstatic --upload-unhashed.files
