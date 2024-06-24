b44aa96dee2080debb22febe5ca131c6deb55c13
tbeleen <T.beleenmiranda@gmail.com>
2024-06-23 20:38:43 -0400

    inicial

--- mysite/settings.py
+++ mysite/settings.py
@@ -163,6 +163,8 @@ cloudinary.config (
     api_secret = 'vVQMxUI15yTInKmcWl-d2-X_kUY'
 )
 
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
